17f492f18e879032263e75d372091051423c53d6
Zeeboi98 <Zayyadm65@gmail.com>
2026-05-19 02:12:54 +0100

    fixed cors - forgot to allow DELETE method

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Movie Sentiment API", version="2.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST", "GET"],
+    allow_methods=["GET", "POST", "DELETE"],
     allow_headers=["*"],
 )
 
